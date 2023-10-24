31c8f18a6549bb3630777dee17f1e0d0ef9d8cd7
Mona Lisa <cordovamonica.a@gmail.com>
2023-10-24 14:24:29 -0500

    more about tweaks

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -1,11 +1,11 @@
 export default function About() {
   return (
-    <div>
+    <div className="lh-lg">
       <p>
         I initially graduated in 2016 with a degree in Psychology. After years
         of not pursuing anything with my degree and bouncing from job to job
         feeling unfulfilled, I decided to make a change. I enrolled in a coding
-        bootcamp thru UTSA and started my path to a career change. Coding is a
+        bootcamp thru University of Texas San Antonio and started my path to a career change. Coding is a
         challenge but having the chance to collaborate with my peers and take
         part in creating or bettering something in the world will have great
         payoff.
@@ -13,10 +13,10 @@ export default function About() {
       <p>
         I am currently based in Dallas, TX. After graduating from the bootcamp,
         I hope to pursue a career as a full stack web developer and continue
-        learning and growing as a developer. I enjoy spoiling my dog, reading,
+        learning and growing as a developer. I enjoy walking, spoiling my dog, reading,
         and games.
       </p>
-      <p>Have a look around and feel free to reach out to get in touch! </p>
+      <p>Have a look around and feel free to reach out and get in touch! </p>
     </div>
   );
 }

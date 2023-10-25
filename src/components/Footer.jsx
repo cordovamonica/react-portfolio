4ed32a4d6d47d028099117c374555447582721bc
Mona Lisa <cordovamonica.a@gmail.com>
2023-10-24 19:03:10 -0500

    added icons to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import { FaGithub } from 'react-icons/fa';
+import { FaLinkedin } from 'react-icons/fa';
+import { FaGoodreads } from 'react-icons/fa';
 
 export default function Footer() {
   return (
     <footer>
-      <p className='text-center'>&copy; 2023</p>
+      <div className='text-center fs-2 grid gap-3'>
+      <a className="p-2 g-col-6 " href="https://github.com/cordovamonica"><i><FaGithub /></i></a>
+      <a className="p-2 g-col-6" href="https://www.linkedin.com/in/monica-cordova-525233298/"><i><FaLinkedin /></i></a>
+      <a className="p-2 g-col-6" href="https://www.goodreads.com/user/show/170294001-monica"><i><FaGoodreads /></i></a>
+      </div>
     </footer>
   );
 }

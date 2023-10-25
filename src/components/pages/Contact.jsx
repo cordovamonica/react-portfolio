c2efc60a2fd587356fb592b9f09830615d2b383a
Mona Lisa <cordovamonica.a@gmail.com>
2023-10-24 23:35:06 -0500

    added rough contact form

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -1,7 +1,61 @@
+import React, { useState } from "react";
+
 export default function Contact() {
-    return (
-        <div>
-            <p>Contact me!</p>
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(`Name: ${name}\nEmail: ${email}\nMessage: ${message}`);
+    setName("");
+    setEmail("");
+    setMessage("");
+  };
+
+  return (
+    <>
+      <div>
+        <h2 className="mx-3 mt-4">Contact</h2>
+      </div>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group mx-3">
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            className="form-control"
+            id="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            required
+          />
         </div>
-    )
+        <div className="form-group mx-3">
+          <label htmlFor="email">Email address</label>
+          <input
+            type="email"
+            className="form-control"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+        <div className="form-group mx-3">
+          <label htmlFor="message">Message</label>
+          <textarea
+            className="form-control"
+            id="message"
+            rows="5"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+            required
+          ></textarea>
+        </div>
+        <button type="submit" className="btn btn-xl btn-outline-dark mx-3 mt-2">
+          Submit
+        </button>
+      </form>
+    </>
+  );
 }
